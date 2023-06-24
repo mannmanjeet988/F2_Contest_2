6388ffb898ae7973a8f490ed270b8ee8d2aac655
mannmanjeet988 <mannmanjeet988@gmail.com>
2023-06-24 23:49:32 +0530

    Chnages

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const keysToDisplay = ['image', 'name', 'symbol', 'id' ,'current_price', 'total_
 
 //Method 1: fetching data using .then method
 
-// fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
-//   .then(response => response.json())
-//   .then(data => {
-//     // Process the fetched data
-//     renderTable(data,keysToDisplay);
-//   })
-//   .catch(error => {
-//     // Handle any errors
-//     console.log(error);
-//   });
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+  .then(response => response.json())
+  .then(data => {
+    // Process the fetched data
+    renderTable(data,keysToDisplay);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.log(error);
+  });
 
 
   function renderTable(data, keys) {
